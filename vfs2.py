176b2b6d8675fd49332d5a6f1cc7fa7e28b2c169
littoma <littoma@163.com>
2018-10-23 20:21:36 -0300

    Finished load_vfs2.

--- vfs2.py
+++ vfs2.py
@@ -6,16 +6,18 @@ class VFS2(object):
     class Folder(object):
         def __init__(self, fs):
             (self.unk1, self.id, 
-            self.unk2, self.unk3, 
+            self.parent, self.unk3, 
             self.file_id_start) = struct.unpack('iiiii', fs.read(struct.calcsize('iiiii')))
-            self.files = []
+            self.entries = []
+            self.name = ''
 
     class File(object):
         def __init__(self, fs):
             (self.unk1, self.id, 
-            self.compress_type, self.folder_id, 
+            self.compress_type, self.parent, 
             self.data_offset, self.data_size) = struct.unpack('iiiiii', fs.read(struct.calcsize('iiiiii')))
-
+            self.name = ''
+        
     def __init__(self, fp=None):
         if fp:
             self.load_vfs2(fp)
@@ -35,14 +37,44 @@ class VFS2(object):
             raise TypeError("Signature error. Except: VFS2, actual: %s"%sig)
 
         folder_count ,= struct.unpack('i', fp.read(4))
-        self.folders = []
+        folders = []
         for i in range(folder_count):
-            self.folders.append(VFS2.Folder(fp))
+            folders.append(VFS2.Folder(fp))
 
         file_count ,= struct.unpack('i', fp.read(4))
-        self.files = []
+        files = []
         for i in range(file_count):
-            self.files.append(VFS2.File(fp))
+            files.append(VFS2.File(fp))
+        
+        self.name_table_offset ,= struct.unpack('i', fp.read(4))
+        self.data_offset = fp.tell()
+
+        fp.seek(self.name_table_offset, 0)
+        filename_count ,= struct.unpack('i', fp.read(4))
+        for i in range(filename_count):
+            str_len ,= struct.unpack('i', fp.read(4))
+            files[i].name = fp.read(str_len)
+        
+        foldername_count ,= struct.unpack('i', fp.read(4))
+        for i in range(foldername_count):
+            str_len ,= struct.unpack('i', fp.read(4))
+            folders[i].name = fp.read(str_len)
+        
+        for i in range(file_count):
+            folder = folders[files[i].parent]
+            if folder.id != files[i].parent:
+                raise ValueError("Folder ID mismatch.")
+            folder.entries.append(files[i])
+        
+        for i in range(folder_count):
+            if folders[i].parent >= 0:
+                folders[folders[i].parent].entries.append(folders[i])
+            elif folders[i].parent == -1:
+                self.tree_root = folders[i]
+
+        fp.seek(self.data_offset, 0)
+        self.fs = fp
+        self.cur_node = self.tree_root
 
     def create_vfs2(self):
         pass
