3674d17d6932570570e225189b44cbda17349c79
littoma <littoma@163.com>
2018-10-23 21:04:46 -0300

    Add change directory and primary list dir function.

--- vfs2.py
+++ vfs2.py
@@ -6,16 +6,18 @@ class VFS2(object):
     class Folder(object):
         def __init__(self, fs):
             (self.unk1, self.id, 
-            self.parent, self.unk3, 
+            self.parent_id, self.unk3, 
             self.file_id_start) = struct.unpack('iiiii', fs.read(struct.calcsize('iiiii')))
             self.entries = []
+            self.parent = None
             self.name = ''
 
     class File(object):
         def __init__(self, fs):
             (self.unk1, self.id, 
-            self.compress_type, self.parent, 
+            self.compress_type, self.parent_id, 
             self.data_offset, self.data_size) = struct.unpack('iiiiii', fs.read(struct.calcsize('iiiiii')))
+            self.parent = None
             self.name = ''
         
     def __init__(self, fp=None):
@@ -61,29 +63,53 @@ class VFS2(object):
             folders[i].name = fp.read(str_len)
         
         for i in range(file_count):
-            folder = folders[files[i].parent]
-            if folder.id != files[i].parent:
+            folder = folders[files[i].parent_id]
+            if folder.id != files[i].parent_id:
                 raise ValueError("Folder ID mismatch.")
             folder.entries.append(files[i])
+            files[i].parent = folder
         
         for i in range(folder_count):
-            if folders[i].parent >= 0:
-                folders[folders[i].parent].entries.append(folders[i])
-            elif folders[i].parent == -1:
+            if folders[i].parent_id >= 0:
+                parent = folders[folders[i].parent_id]
+                parent.entries.append(folders[i])
+                folders[i].parent = parent
+            elif folders[i].parent_id == -1:
                 self.tree_root = folders[i]
+                folders[i].parent = None
 
         fp.seek(self.data_offset, 0)
         self.fs = fp
         self.cur_node = self.tree_root
+        self.cur_depth = 0
 
     def create_vfs2(self):
-        pass
+        raise NotImplementedError("Function not supported.")
     
     def add_file(self, folder, file_path):
-        pass
+        raise NotImplementedError("Function not supported.")
     
     def create_folder(self, folder, name):
-        pass
+        raise NotImplementedError("Function not supported.")
+
+    #cd
+    def change_directory(self, path):
+        if not path:
+            return
+        
+        next_node, next_path = path.split('/', 1)
+        if (not next_node) or (next_node == '.'):
+            return
+        
+        if next_node == '..' and self.cur_node.parent_id != -1:
+            self.cur_node = self.cur_node.parent
+        
+        for e in self.cur_node.entries:
+            if e.name == next_node:
+                self.cur_node = e
+                break
+        
+        return self.change_directory(next_path)
     
     def exists(self, path):
         pass
@@ -91,8 +117,9 @@ class VFS2(object):
     def extract(self, out_dir):
         pass
     
-    def list(self):
-        pass
+    def list_dir(self):
+        for e in self.cur_node.entries:
+            print e.name
 
     def save(self, path):
         pass
@@ -100,4 +127,5 @@ class VFS2(object):
 
 if '__main__' == __name__:
     vfs = VFS2('data.vfs')
-    print vfs
+    vfs.change_directory('fonts/')
+    vfs.list_dir()
